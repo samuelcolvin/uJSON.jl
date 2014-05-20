73220b0e860ee21df46eaae9cf21ddf75d5051f5
Samuel Colvin <s@muelcolvin.com>
2014-05-20 02:32:24 +0100

    again trying to get travis working

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,17 +2,10 @@ using uJSON
 using JSON
 using Base.Test
 
-function print_sum(a)
-    print(summary(a), ": ")
-    show(a)
-    println()
-end
-
-print("uJSON running trivial example:")
-input_str = "[1,2,\"hello\", null, 3.2123]"
+println("Running trivial example: ")
+input_str = "[1,2,\"hello\", null, 3.2123, {\"a\":1}]"
 uresult = uJSON.parse(input_str)
-print(input_str, " >> ")
-print_sum(uresult)
+println(input_str, " >> ", uresult)
 
 fn = Pkg.dir("uJSON", "test", "sample.json")
 print("\nJSON running simple test:  ")
